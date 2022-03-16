5fc19a063ae8d30fab051bcdab84968fdbbf987d
Peter Vercauteren <peter@pixeliner.com>
2022-03-16 01:17:59 +0100

    add apollo driver

--- src/graphql/graphql-wrapper.module.ts
+++ src/graphql/graphql-wrapper.module.ts
@@ -1,9 +1,15 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
 import { GqlConfigService } from './config';
 
 @Module({
-  imports: [GraphQLModule.forRootAsync({ useClass: GqlConfigService })],
+  imports: [
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useClass: GqlConfigService,
+    }),
+  ],
 })
 export class GraphqlWrapperModule {}
